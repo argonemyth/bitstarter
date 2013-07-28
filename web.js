1cceb3465f64e7a9947714c044c27360d81a64ac
Fei Tan <fei@argonemyth.com>
2013-07-28 17:00:37 +0400

    Modified web.js so it runs on port 8080.

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var content = fs.readFileSync('index.html').toString();
-  response.send(content);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT ||8080; 
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
